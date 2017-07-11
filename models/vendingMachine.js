439c39a36922c1719e18b476a965097560c83df9
Alan Richards <alanrichards86@gmail.compwdlsexitstopopengitopengit config --global user.name Alan>
2017-07-11 12:00:34 -0400

    Just addin this

--- models/vendingMachine.js
+++ models/vendingMachine.js
@@ -6,6 +6,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/vendingMachine');
 
 
 
+
+
 // const costomerSchema = new Schema({
 //   _id: {type: Number, required: true, default: 1}
 //   account: Number,
